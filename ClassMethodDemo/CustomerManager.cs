bf547326eb6450a2cbfb94abcde2d1e29866075a
fatih demirören <afatih9@windowslive.com>
2021-01-17 23:48:30 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void CustomerAdd(Customer customer) 
+        {
+            Console.WriteLine("Kayıt Başarılı :" + customer.Adi + " " + customer.Soyadi);        
+        }
+
+        public void CustomerDelete(Customer customer) 
+        {
+            Console.WriteLine("Kayıt Silindi :" + customer.Adi + " " + customer.Soyadi);       
+        }
+
+        public void CustomerList(Customer customer) 
+        {
+            
+            Console.WriteLine("Müşteri Listesi :" + customer.Id + " " + customer.Adi + " " + customer.Soyadi + " " + customer.KrediNotu);
+        
+        }
+
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 0956;
+            customer1.Adi = "Sinem";
+            customer1.Soyadi = "Ay";
+            customer1.KrediNotu = "C";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 0568;
+            customer2.Adi = "Beren";
+            customer2.Soyadi = "Venüs";
+            customer2.KrediNotu = "A";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 0486;
+            customer3.Adi = "Fatih";
+            customer3.Soyadi = "Mars";
+            customer3.KrediNotu = "B";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            Console.WriteLine("MÜŞTERİ LİSTESİ");
+
+            foreach (var custom in customers)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Müşteri Id : " + custom.Id);
+                Console.WriteLine("Adı : " + custom.Adi);
+                Console.WriteLine("Soyadı : " + custom.Soyadi);
+                Console.WriteLine("Kredi Notu : " + custom.KrediNotu);
+
+            }
+
+            Console.WriteLine("---------------------------");
+
+            CustomerManager customer = new CustomerManager();
+            customer.CustomerAdd(customer1);
+            customer.CustomerAdd(customer2);
+            customer.CustomerAdd(customer3);
+
+            Console.WriteLine("---------------------------");
+
+            customer.CustomerDelete(customer1);
+            customer.CustomerDelete(customer2);
+            customer.CustomerDelete(customer3);
+
+            Console.WriteLine("-----------------------------");
+
+           
+
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
